8e096122e40a84d564b52f9fdaf489579b17f697
nemojn <nemo@eliworks.com>
2024-03-10 08:37:12 -0400

    Initial commit. Not in working state yet.

---
+++ homepage.php
@@ -0,0 +1,13 @@
+<?php
+	echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
+	echo '<pre class="header">
+     ____    ____    _____                         
+    |  _ \  |  _ \  |  ___|  _____   _   _   _____ 
+    | |_> } | |_> } | |___  |  _  | | | | | |  _  |
+    |  _ <  |  _ <  |___  | | |_| | | | | | | |_| |
+    | |_> } | |_> }  ___| | |___  | | |_| | |  ___|
+    |____/  |____/  |_____|     |_| |_____| |_____|
+    </pre>';
+	echo '<br>';
+	echo '<pre> welcome $name !'</pre>;
+?>
---
+++ loggedout.php
@@ -0,0 +1,4 @@
+<?php
+	echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
+	echo '<pre>logged out successfully!</pre>';
+?>
